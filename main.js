aa1eca635a97f70d51741393f6005422c680bd96
Carlos Allende <callende@Carlos-Allendes-mac-mini.local>
2017-02-17 11:28:17 -0800

    changing api to zipcodesapi

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-console.log('loaded')
+console.log('loaded2')
 
 var lat;
 var lng;
-$('#sendAddress').click(function(e) {
-  e.preventDefault();
+
+$('#sendAddress').click(function() {
   $('#postalCodesWrapper').addClass('hidden');
   $('#postalCodes').empty();
   var address = document.getElementById('address').value;
   var radius = Math.round(document.getElementById('radius').value*1.61);
   var numberOfResults = document.getElementById('numberOfResults').value || 500;
   var addressForGoogle = encodeURIComponent(address);
+  var zipCode;
   $.ajax({
   url: "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&sensor=false",
   success: function(response){
-      lat = response.results[0].geometry.location.lat;
-      lng = response.results[0].geometry.location.lng;
-      console.log("lat= ",lat);
-      console.log("lng= ",lng);
+      zipCode = response.results[0].address_components[7].long_name;
     }
   }).then(function() {
     $.ajax({
-      url: "http://api.geonames.org/findNearbyPostalCodesJSON?lat="+lat+"&lng="+lng+"&maxRows="+numberOfResults+"&radius="+radius+"&username=vanpeta",
+      url: "https://www.zipcodeapi.com/rest/LZ6oL7k2QGYDanDX9bA9AuQaGK2F51MR1SfMZzOQgNuqa7nkjYuTsl4966Ob7B28/radius.json/"+zipCode+"/"+radius+"/km",
       success: function (response) {
+        console.log(response)
         $('#postalCodesWrapper').removeClass('hidden');
         response.postalCodes.forEach(function(e) {
           $('#postalCodes').append("<div class='code'>'"+e.postalCode+"', </div>");
@@ -32,5 +31,3 @@ $('#sendAddress').click(function(e) {
     });
   });
 });
-
-//LZ6oL7k2QGYDanDX9bA9AuQaGK2F51MR1SfMZzOQgNuqa7nkjYuTsl4966Ob7B28
