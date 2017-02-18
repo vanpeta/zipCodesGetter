4c937dfe1e19a612a96a9421d009950c3e98fb20
Carlos Allende <carlosallendesanz@gmail.com>
2017-02-17 23:53:05 -0800

    adding heroku port to app.listen

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
